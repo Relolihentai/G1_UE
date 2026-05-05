d4a91888c474db4f5099fcd4ec13fe00f310a641
Relolihentai <1347264801@qq.com>
2026-05-05 18:27:21 +0800

    Initial Unreal project

---
+++ Source/G1_UE/G1_UE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "G1_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, G1_UE, "G1_UE" );
